e50d6ede9825e8e00b43a3a5b0e87ce193b522df
Rosa Durante <rosa.durante@hogarthww.com>
2013-09-04 18:02:15 +0100

    Fixed admin section

--- to_do/urls.py
+++ to_do/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, include, url
 from task.views import TaskList
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', TaskList.as_view(), name='TaskList'),
