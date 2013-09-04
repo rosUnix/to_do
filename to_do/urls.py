a75cb57e0a7d4d62c10f06ab0bf8da467fdc1c12
Rosa Durante <rosa.durante@hogarthww.com>
2013-09-04 16:49:29 +0100

    Adding the url of the project and acces to admin

--- to_do/urls.py
+++ to_do/urls.py
@@ -1,17 +1,11 @@
 from django.conf.urls import patterns, include, url
+from task.views import TaskList
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'to_do.views.home', name='home'),
-    # url(r'^to_do/', include('to_do.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', TaskList.as_view(), name='TaskList'),
+    url(r'^admin/', include(admin.site.urls)),
 )
