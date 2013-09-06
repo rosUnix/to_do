b332d8bdefd5486623e88ff78d4609248f64ff7c
Rosa Durante <rosa.durante@hogarthww.com>
2013-09-06 09:24:09 +0100

    Adding new url for ajax calls.

--- to_do/urls.py
+++ to_do/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from task.views import TaskList
+from task.views import TaskList, TaskView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -7,5 +7,21 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', TaskList.as_view(), name='TaskList'),
+    url(r'^task/', TaskView.as_view(), name='TaskView'),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+"""
+MyModel = Backbone.Model.extend({
+
+url: function(){ "API/"
+      return "API/MyModel/" +this.get("id");
+    }
+});
+
+MyCollection = Backbone.Collection.extend({
+    model: MyModel ,
+    url: "API/MyModels"
+});
+"""
