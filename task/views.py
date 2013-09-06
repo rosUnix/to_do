25a9c536a32a159bf5f1bcb9b69a7fc48de18841
Rosa Durante <rosa.durante@hogarthww.com>
2013-09-06 12:07:57 +0100

    Adding new view for ajax request. Send all list of tasks in JSON

--- task/views.py
+++ task/views.py
@@ -1,4 +1,6 @@
 # Create your views here.
+import json
+from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 
@@ -15,3 +17,14 @@ class TaskList(ListView):
 
 class TaskView(UpdateView):
     model = Task
+
+
+def get_task_list(request):
+
+    taskList = [{'id': task.id,
+                'title': task.title,
+                'desc': task.description,
+                'created_at': task.created_at.strftime("%D"),
+                'status': task.status} for task in Task.objects.all()]
+
+    return HttpResponse(json.dumps(taskList), content_type="application/json")
