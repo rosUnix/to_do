8501614e9d9d6aaffbff71bb3985ea1a4e27f599
Rosa Durante <rosa.durante@hogarthww.com>
2013-09-06 09:23:21 +0100

    Adding TaskView

--- task/views.py
+++ task/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
 from django.views.generic.list import ListView
+from django.views.generic.edit import UpdateView
 
 from task.models import Task
 
@@ -7,3 +8,10 @@ from task.models import Task
 class TaskList(ListView):
     model = Task
     template_name = 'task/list.html'
+
+    # def get(self, request, *args, *kwargs):
+    #    pass
+
+
+class TaskView(UpdateView):
+    model = Task
