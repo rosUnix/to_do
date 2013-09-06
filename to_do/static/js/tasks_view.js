61c111ff6a49d0ae7a5788daa5fc9557b97da9cd
Rosa Durante <rosa.durante@hogarthww.com>
2013-09-06 12:11:24 +0100

    First functions on JS. Defined model, collection and view for one task and the main view to handle behaviour on them. Initialize collection and fetch all tasks we had in db

--- to_do/static/js/tasks_view.js
+++ to_do/static/js/tasks_view.js
@@ -1,58 +1,77 @@
 define('tasks_view', ['app'], function (app) {
 
-	var task_model = Backbone.Model.extend({}),
-			// url: function () {
-			// 	return 'task/' + this.get('id');
-			// }
-		// }),
+	var TaskModel = Backbone.Model.extend({
+			url: function () {
+				return '/task/' + this.get('id');
+			}
+		}),
 
-		task_view = Backbone.View.extend({
+		TasksCollection = Backbone.Collection.extend({
+			url: function () {
+				return 'tasks/';
+			}
+		}),
+
+		TaskView = Backbone.View.extend({
 			tagName: 'li',
-			template: '',
+			template: '' +
+				'<p><input type="checkbox" name="task" value="<%= id %>" /></p>' +
+				'<p><%= title %></p>' +
+				'<p><%= desc %></p>' +
+				'<p><%= created_at %></p>',
 
 			initialize: function () {},
 			render: function () {
-				this.$el.html(this.template(this.model.toJSON()));
+
+				this.$el.html(_.template(this.template, this.model.toJSON()));
+				this.$el.attr('task_id', this.model.get('id'));
 				return this;
 			},
 		});
 
+
 	return Backbone.View.extend({
-		tagName: 'ul',
 		noTaskTemplate: '<li class="no-task">There is not task to do! YaY! </li>',
 
 		initialize: function (options) {
+			var self = this;
 			this.parent = options.parent;
+			this.broker = options.broker;
 
-			// Set up listing tasks.
-			// Fetch collection of tasks.
-
-			this.tasksCollection = Backbone.Collection.extend({
-				model: task_model,
-				url: '/tasks'
+			// Define a collection where will be stored the list of tasks.
+			this.tasksCollection = new TasksCollection({
+				model: TaskModel
 			});
 
-
-			// this.tasksCollection = Backbone.Collection.extend({
-			// 	model: task_model,
-			// 	url: 'tasks/'
-			// });
-
-			this.tasksCollection.fetch();
-
-			if (this.$el.children().length !== this.tasksCollection.length && !this.$el.find('no-task').length) {
-				this.render();
-			}
-
-			this.bind('sync', function (e) {
-				console.log('sync event!');
+			// Getting the list! Make an initial request to the backend
+			// to store all task in the collection as models.
+			this.tasksCollection.fetch({
+				success: function () {
+					if (self.$el.children().length !== self.tasksCollection.length &&
+						!self.$el.find('.no-task').length) {
+						self.render();
+					}
+				}
 			});
+
+			// Binding events!
+			this.broker.on('task:create', this.addTask, this);
+			this.broker.on('task:edit:view', this.editTasks, this);
+			this.broker.on('task:edit:save', this.saveTasks, this);
+			this.broker.on('task:status:change', this.changeStatusTasks, this);
+			this.broker.on('task:remove', this.removeTasks, this);
+			this.broker.on('task:search', this.searchTasks, this);
 		},
 
 		render: function () {
-			this.$el.remove();
+			this.$el.children().remove();
 			this.tasksCollection.each(function (model) {
-				var view = new task_view({model: model});
+				
+				var view = new TaskView({
+					model: model,
+					className: 'backbone tag ' + model.get('status')
+				});
+
 				this.$el.append(view.render().el);
 			}, this);
 
@@ -70,9 +89,11 @@ define('tasks_view', ['app'], function (app) {
 			});
 		},
 
-		_syncModel: function (model, response) {
-			model.syncNew(response);
-		}
+		editTasks: function (listTasks) {},
+		saveTasks: function (listTasks) {},
+		changeStatusTasks: function (listTasks) {},
+		removeTasks: function (listTasks) {},
+		searchTasks: function (query) {}
 	});
 
 });
