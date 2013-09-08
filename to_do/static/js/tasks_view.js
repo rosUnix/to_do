8df08cc26e537739955f73c841dbbe11818bdf2a
Rosa Durante <rosamaria.durante@gmail.com>
2013-09-08 17:33:21 +0100

    Adding functionality on checked tasks.

--- to_do/static/js/tasks_view.js
+++ to_do/static/js/tasks_view.js
@@ -1,12 +1,16 @@
 define('tasks_view', ['app'], function (app) {
 
 	var TaskModel = Backbone.Model.extend({
+			defaults: {
+				'selected': false
+			},
 			url: function () {
 				return '/task/' + this.get('id');
 			}
 		}),
 
 		TasksCollection = Backbone.Collection.extend({
+			model: TaskModel,
 			url: function () {
 				return 'tasks/';
 			}
@@ -20,13 +24,21 @@ define('tasks_view', ['app'], function (app) {
 				'<p><%= desc %></p>' +
 				'<p><%= created_at %></p>',
 
-			initialize: function () {},
+			initialize: function () {
+				_.bindAll(this, 'selectTask');
+				this.$el.find('input').bind('click', this.selectTask);
+			},
+
 			render: function () {
 
 				this.$el.html(_.template(this.template, this.model.toJSON()));
 				this.$el.attr('task_id', this.model.get('id'));
 				return this;
 			},
+
+			selectTask: function (e) {
+				this.model.set('selected', !this.model.get('selected'));
+			}
 		});
 
 
@@ -38,10 +50,10 @@ define('tasks_view', ['app'], function (app) {
 			this.parent = options.parent;
 			this.broker = options.broker;
 
+			_.bindAll(this, 'selectItems');
+
 			// Define a collection where will be stored the list of tasks.
-			this.tasksCollection = new TasksCollection({
-				model: TaskModel
-			});
+			this.tasksCollection = new TasksCollection();
 
 			// Getting the list! Make an initial request to the backend
 			// to store all task in the collection as models.
@@ -50,9 +62,20 @@ define('tasks_view', ['app'], function (app) {
 					if (self.$el.children().length !== self.tasksCollection.length &&
 						!self.$el.find('.no-task').length) {
 						self.render();
+					} else {
+						self.tasksCollection.each(function (model) {
+							// Binding events. Need to create the views.
+							var view = new TaskView({
+								el: self.$el.find('[task_id=' + model.get('id') + ']'),
+								model: model,
+								className: 'backbone tag ' + model.get('status')
+							});
+						}, this);
 					}
 				}
 			});
+
+			this.tasksCollection.bind('change:selected', this.selectItems);
 
 			// Binding events!
 			this.broker.on('task:create', this.addTask, this);
@@ -80,6 +103,19 @@ define('tasks_view', ['app'], function (app) {
 			}
 
 			return this;
+		},
+
+		selectItems: function (model) {
+			// If an item has been checked and it's the first one: trigger an event to enable editRemove mode.
+			if (model.get('selected') && this.tasksCollection.where({selected: true}).length === 1) {
+				this.broker.trigger('task:select', 'checked');
+			}
+
+			// Else, if an item has been unchecked and it was the last element checked: trigger an event to disable
+			// editRemove mode.
+			else if (!model.get('selected') && !this.tasksCollection.where({selected: true}).length) {
+				this.broker.trigger('task:select', 'unchecked');
+			}
 		},
 
 		addTask: function (object) {
