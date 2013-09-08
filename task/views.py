06abbd3121d6d56ab1944848be358cf44285f0f9
Rosa Durante <rosamaria.durante@gmail.com>
2013-09-08 19:08:44 +0100

    Adding tasks on the backend side - Adding a new function for a POST request.

--- task/views.py
+++ task/views.py
@@ -1,8 +1,10 @@
 # Create your views here.
+import datetime
 import json
+
 from django.http import HttpResponse
 from django.views.generic.list import ListView
-from django.views.generic.edit import UpdateView
+from django.views.decorators.csrf import csrf_exempt
 
 from task.models import Task
 
@@ -11,20 +13,29 @@ class TaskList(ListView):
     model = Task
     template_name = 'task/list.html'
 
-    # def get(self, request, *args, *kwargs):
-    #    pass
-
-
-class TaskView(UpdateView):
-    model = Task
-
 
 def get_task_list(request):
 
     taskList = [{'id': task.id,
                 'title': task.title,
-                'desc': task.description,
+                'description': task.description,
                 'created_at': task.created_at.strftime("%D"),
                 'status': task.status} for task in Task.objects.all()]
 
     return HttpResponse(json.dumps(taskList), content_type="application/json")
+
+
+@csrf_exempt
+def create_task(request):
+
+    if request.method == 'POST':
+        datas = json.loads(request.body)
+        datas['created_at'] = datetime.datetime.now().strftime("%Y-%m-%d")
+
+        newTask = Task.objects.create(
+            title=datas['title'],
+            description=datas['description'],
+            status=datas['status'],
+            created_at=datas['created_at'])
+
+        return HttpResponse(json.dumps({'id': newTask.id, 'created_at': newTask.created_at}), content_type="application/json");
