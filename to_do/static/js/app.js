02d95abdea31883ad6f93538d8d87f6c7dcad71a
Rosa Durante <rosa.durante@hogarthww.com>
2013-09-09 11:50:38 +0100

    Removing comments

--- to_do/static/js/app.js
+++ to_do/static/js/app.js
@@ -21,15 +21,6 @@ define('app', ['Backbone', 'underscore', 'tasks_view', 'nav_view'], function (Ba
 				el: this.$el.find('nav'),
 				broker: this.broker
 			});
-
-			// Events:
-			// this.broker.on('task:create', this.addTask, this);
-			// this.broker.on('task:edit:view', this.editTasks, this);
-			// this.broker.on('task:edit:save', this.saveTasks, this);
-			// this.broker.on('task:status:change', this.changeStatusTasks, this);
-			// this.broker.on('task:remove', this.removeTasks, this);
-			//
-			// this.broker.on('task:select', this.selectedItem, this);
 		}
 	});
 });
