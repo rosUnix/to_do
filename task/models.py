1557de38bcc9fa4099655c210d7e2daf7c19d715
Rosa Durante <rosa.durante@hogarthww.com>
2013-09-09 11:48:56 +0100

    Set order getting the list of tasks

--- task/models.py
+++ task/models.py
@@ -1,11 +1,16 @@
+import datetime
+
 from django.db import models
 from django.conf import settings
 
 
 class Task(models.Model):
     title = models.CharField(max_length=50, unique=True)
-    created_at = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=30, choices=settings.TASK_CHOICES)
+
+    class Meta:
+        ordering = ('-created_at',)
 
     def __unicode__(self):  # pragma: no cover
         return self.title
