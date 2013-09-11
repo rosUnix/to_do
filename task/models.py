9dc15373c9c8a89b5705c01d882a1b901d9c0a7e
Rosa Durante <rosamaria.durante@gmail.com>
2013-09-11 21:13:46 +0100

    Adding more characters for a task title

--- task/models.py
+++ task/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 
 class Task(models.Model):
-    title = models.CharField(max_length=50, unique=True)
+    title = models.CharField(max_length=250, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=30, choices=settings.TASK_CHOICES)
 
